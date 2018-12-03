f2e8dc883e980cea23ae87ee36775a2846ac391d
Valentin <vasnake@gmail.com>
2018-12-03 16:55:33 +0300

    fix quoted fields separator

--- leaflet.geocsv-src.js
+++ leaflet.geocsv-src.js
@@ -29,7 +29,8 @@ L.GeoCSV = L.GeoJSON.extend({
     fieldSeparator: ';',
     lineSeparator: '\n',
     deleteDoubleQuotes: true,
-    firstLineTitles: false
+    firstLineTitles: false,
+    quoteSymbol: ''
   },
 
   _propertiesNames: [],
@@ -87,6 +88,21 @@ L.GeoCSV = L.GeoJSON.extend({
     return cadena;
   },
 
+  _splitLine: function (rawline) {
+    if (this.options.quoteSymbol == '') {
+      return rawline.split(this.options.fieldSeparator).map(s => s.trim());
+    }
+
+    const replaceSymbol = '\n';
+    const re1 = new RegExp(this.options.quoteSymbol + this.options.fieldSeparator, 'gi');
+    const re2 = new RegExp(replaceSymbol, 'gi');
+    // hide quoted fieldSeparator
+    const line = rawline.replace(re1, replaceSymbol);
+    const lst = line.split(this.options.fieldSeparator);
+    // and than restore it back
+    return lst.map(s => s.replace(re2, this.options.fieldSeparator).trim());
+  },
+
   _csv2json: function (csv) {
     var json = {};
     json["type"]="FeatureCollection";
@@ -95,7 +111,7 @@ L.GeoCSV = L.GeoJSON.extend({
 
     csv = csv.split(this.options.lineSeparator);
     for (var num_linea = 0; num_linea < csv.length; num_linea++) {
-      var campos = csv[num_linea].trim().split(this.options.fieldSeparator)
+      var campos = this._splitLine(csv[num_linea])
         , lng = parseFloat(campos[titulos.indexOf(this.options.longitudeTitle)])
         , lat = parseFloat(campos[titulos.indexOf(this.options.latitudeTitle)]);
       if (campos.length==titulos.length && lng<180 && lng>-180 && lat<90 && lat>-90) {
@@ -112,7 +128,10 @@ L.GeoCSV = L.GeoJSON.extend({
           }
         }
         json["features"].push(feature);
-      } 
+      }
+      else {
+        console.log(`bad record ${num_linea} '${csv[num_linea]}'`);
+      }
     }
     return json;
   }
